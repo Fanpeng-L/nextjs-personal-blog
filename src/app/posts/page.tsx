931735cf555fd5c89bb85438dc9179894bc6b127
Fanpeng Li <serforfp@gmail.com>
2024-05-27 17:59:34 +0100

    Fetched dummy data from API with server component

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,14 +1,26 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "All Posts",
   description: "All posts",
 };
 
-export default function Page() {
+export default async function Page() {
+  const response = await fetch("https://dummyjson.com/posts?limit=10");
+  const data = await response.json();
+
   return (
-    <main className="text-center pt-32 px-5">
-      <h1 className="text-4xl md:text-5xl font-bold mb-5">All Posts</h1>
+    <main className="text-center pt-16 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-6">All Posts</h1>
+
+      <ul>
+        {data.posts.map((post) => (
+          <li key={post.id} className="mb-4">
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
